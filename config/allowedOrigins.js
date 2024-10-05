f757eab5ca5c73804d0e43649510e2a5335478ac
ahsan <makabir98@yahoo.com>
2024-10-06 01:53:08 +0600

    allowedOrigin updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://vehicle-reservation-react.vercel.app'
 ];
 
 module.exports = allowedOrigins;
